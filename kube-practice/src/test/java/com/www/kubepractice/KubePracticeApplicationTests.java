c314b0acca3dc0a8c7847563a1838cabe409c5f3
bboodd <gkrgus9@gmail.com>
2025-12-31 15:15:47 +0900

    pod

---
+++ kube-practice/src/test/java/com/www/kubepractice/KubePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.www.kubepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubePracticeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
